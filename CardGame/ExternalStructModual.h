e030618600151026a796f9210843f4376197e2ae
redstonedash <redatonedash@gmail.com>
2020-01-22 22:00:18 -0800

    added code from home

--- CardGame/ExternalStructModual.h
+++ CardGame/ExternalStructModual.h
@@ -1,3 +1,10 @@
+#pragma once
 flecs::component<Transform> Transform_c(world, "Transform");
 flecs::component<Model> Model_c(world, "Model");
 flecs::component<BoundingBox> BoundingBox_c(world, "BoundingBox");
+flecs::component<Shader> Shader_c(world, "Shader");
+flecs::component<Color> Color_c(world, "Color");
+flecs::component<Rectangle> Rectangle_c(world, "Rectangle");
+flecs::component<RenderTexture> RenderTexture_c(world, "RenderTexture");
+flecs::component<Matrix> Matrix_c(world, "Matrix");
+flecs::component<Camera> Camera_c(world, "Camera");
