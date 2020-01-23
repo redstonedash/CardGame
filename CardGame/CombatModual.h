8c99644ad3a70da5e2f82cfd392cedb71835c9f5
Carter <carter.patterson95@gmail.com>
2020-01-22 17:53:00 -0800

    FUCKKK

--- CardGame/CombatModual.h
+++ CardGame/CombatModual.h
@@ -3,6 +3,11 @@ struct Attack {
 	bool isLeft;
 };
 
+struct Vec2 {
+	int x;
+	int y;
+};
+
 flecs::entity HandP1[10];
 flecs::entity HandP2[10];
 flecs::entity Board[2][5];
@@ -21,18 +26,8 @@ auto registerAttackSystem = [&]() {
 			auto sde = side[row];
 			auto atk = attack[row];
 
-			int i = 0;
 
-			if (sde.isP1) {
-				while (rows.entity(row).id() != Board[0][i].id()) {
-					i++;
-				}
-			}
-			else {
-				while (rows.entity(row).id() != Board[1][i].id()) {
-					i++;
-				}
-			}
+
 		}
 	});
 };
@@ -41,10 +36,30 @@ auto registerAttackSystem = [&]() {
 
 ///////////////////////FUNCTIONS///////////////////////
 
-TargetInfo TargetsExist() {
-
+Vec2 FindMe(flecs::entity_t id) {
+	int i = 0;
+	int j = 0;
+	for (; id != Board[i][j].id(); j++) {
+		if (j >= 5) {
+			j = 0;
+			i = 1;
+		}
+	}
+	return { i, j };
 }
 
-int FindMe() {
+TargetInfo TargetsExist(flecs::entity_t id) {
+	Vec2 data = FindMe(id);
+	bool isP1, isEdge;
+	TargetInfo returnData = {false, false};
+
+	if (data.x == 0) isP1 = true;
+	if (data.y == 0) isEdge = true;
+
+	if (isP1) {
+		if (!isEdge) {
+			
+		}
+	}
 
 }
