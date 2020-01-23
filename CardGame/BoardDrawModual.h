2d07ff0138be62d277db45184d199f6120e6f769
Carter <carter.patterson95@gmail.com>
2020-01-23 14:26:25 -0800

    OOPS

--- CardGame/BoardDrawModual.h
+++ CardGame/BoardDrawModual.h
@@ -3,8 +3,9 @@
 flecs::system<Card> DrawCardSystem_s(world, "DrawCardSystem");
 auto registerDrawCardSystem = [&]() {
 	DrawCardSystem_s.kind(flecs::Manual);
-	DrawCardSystem_s.action([&](flecs::rows rows) {
+	DrawCardSystem_s.action([&](flecs::rows rows, flecs::column<Card> card) {
 		for (auto row : rows) {
+			rows.entity(row).id();
 		}
 	});
 };
