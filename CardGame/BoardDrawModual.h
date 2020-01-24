98ebf67ad7cee13fe623e2994ac936a6089aa4e7
Carter <carter.patterson95@gmail.com>
2020-01-23 17:20:24 -0800

    Update BoardDrawModual.h

--- CardGame/BoardDrawModual.h
+++ CardGame/BoardDrawModual.h
@@ -5,13 +5,21 @@ auto registerDrawCardSystem = [&]() {
 	DrawCardSystem_s.kind(flecs::OnStore);
 	DrawCardSystem_s.action([&](flecs::rows rows, flecs::column<Card> card) {
 		for (auto row : rows) {
-			
 			Vec2 data = FindMe(rows.entity(row).id());
 			if (data.x == 0) {
 				DrawModel(cardModel, { data.y * 0.005f - 0.01115f, 0, .0025f }, .00075f, RED);
 			} else {
 				DrawModel(cardModel, { data.y * -0.005f + 0.01115f, 0, -.0025f }, .00075f, BLUE);
 			}
+		}
+	});
+};
+
+flecs::system<Card> DrawCardInfoSystem_s(world, "DrawCardInfoSystem");
+auto registerDrawCardInfoSystem = [&]() {
+	DrawCardInfoSystem_s.kind(flecs::OnStore);
+	DrawCardInfoSystem_s.action([&](flecs::rows rows, flecs::column<Card> card) {
+		for (auto row : rows) {
 		}
 	});
 };
